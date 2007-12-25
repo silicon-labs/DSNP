c43c03ffc43a51f382771968cbfa0eaffcc8b25d
Adrian Thurston <thurston@complang.org>
2007-12-25 23:14:51 +0000

    First cut of the protocol description. In returnrelid.php Import the id.asc sooner. Added an ISC license (http://en.wikipedia.org/wiki/ISC_license). The http get timeout is now controlled by one var in lib/iduri.php. Upped it to 5 seconds.

--- returnrelid.php
+++ returnrelid.php
@@ -1,5 +1,21 @@
 <?php
 
+/* 
+ * Copyright (c) 2007, Adrian Thurston <thurston@cs.queensu.ca>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 include('config.php');
 include('lib/iduri.php');
 
@@ -9,11 +25,14 @@ requireOwner();
 
 $furi = $_GET['uri'];
 
+# Get the public key of the friend.
+$gnupg = new gnupg();
+$fp = import_id( $gnupg, $furi, $HTTP_GET_TIMEOUT );
+
 # Fetch and decrypt the relid from the potential friend.
 $asc = $furi . 'getrelid.php?fp=' . $FINGERPRINT;
-$response = http_get( $asc, array("timeout"=>1), $info );
+$response = http_get( $asc, array("timeout"=>$HTTP_GET_TIMEOUT), $info );
 $message = http_parse_message( $response );
-$gnupg = new gnupg();
 $gnupg->setsignmode( gnupg::SIG_MODE_NORMAL );
 $gnupg->adddecryptkey( $FINGERPRINT, "" );
 $getrelid = "";
@@ -21,9 +40,6 @@ $res = $gnupg->decryptverify( $message->body, $getrelid );
 
 # Create a relationship id for the friend to use.
 $putrelid = sha1( uniqid( mt_rand() ) );
-
-# Get the public key of the friend.
-$fp = import_id( $gnupg, $furi );
 
 # Store the fingerprint and the relids. 
 $data = read_data();

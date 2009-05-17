9d067c3c0351b22fb0d87ec75f8df4fbfd17bebb
Adrian Thurston <thurston@complang.org>
2009-05-17 02:46:44 +0000

    updated error strings

--- php/error.php
+++ php/error.php
@@ -3,5 +3,11 @@
 $ERROR[1] = 'A friend claim already exists.';
 $ERROR[2] = 'A friend request already exists.';
 $ERROR[3] = 'Could not fetch public key.';
+$ERROR[4] = 'Could not fetch the relid that was requested.';
+$ERROR[5] = 'Failed to decrypt and verify the message.';
+$ERROR[6] = 'Failed to encrypt and sign the message.';
+$ERROR[7] = 'The decrypted message was the wrong size';
+$ERROR[8] = 'Could not fetch the response relid.';
+$ERROR[9] = 'The relid that was returned did not match the original we chose.';
 
 ?>
